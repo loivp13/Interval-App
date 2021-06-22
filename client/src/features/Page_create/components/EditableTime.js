e813b477f51e623a33b70eb81ad2316afb1d4f2f
Loi <loivp13@gmail.com>
2021-06-22 16:17:11 -0700

    installed lodash, created state for input on scroll

--- client/src/features/Page_create/components/EditableTime.js
+++ client/src/features/Page_create/components/EditableTime.js
@@ -1,8 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import classNames from "classnames";
+import { selectTimer } from "../../Page_ActiveTimer/components/timerSlice";
+import debounce from "lodash/debounce";
 
 export default function EditableTime() {
   let timeValuesClassNames = classNames("h-1/3", "scroll-child-start");
+  let [selectedTime, setSelectedTime] = useState({ min: 0, sec: 1 });
+  let [scrollTopValue, setScrollTopValue] = useState({
+    scrollTopMin: 0,
+    scrollTopSec: 0,
+  });
 
   //Add extra divs
   const addMoreColumns = (type, arr) => {
@@ -31,20 +38,78 @@ export default function EditableTime() {
     addMoreColumns("min", minColumns);
     return minColumns;
   };
+
   const [secColumnState, setSecColumn] = useState(renderSecColumns());
   const [minColumnState, setMinColumn] = useState(renderMinColumns());
+
+  const checkShouldAddColumn = (e) => {
+    let { scrollTop, clientHeight, scrollHeight } = e.target;
+    if (scrollTop + clientHeight === scrollHeight) {
+      console.log(scrollTop, clientHeight, scrollHeight);
+      return { type: "append", shouldAddColumn: true, scrollTop: scrollHeight };
+    } else if (scrollTop === 0) {
+      console.log(scrollTop, clientHeight, scrollHeight);
+
+      return { type: "prepend", shouldAddColumn: true, scrollTop: 0 };
+    } else {
+      return { type: "", shouldAddColumn: false };
+    }
+  };
+  //calculate and update timer state
+  const debounceCalcSecChange = debounce(function (scrollTop, childBoxHeight) {
+    let difference =
+      Math.round(
+        (scrollTop - previousScrollTopRefSec.current) / childBoxHeight
+      ) % 60;
+    if (difference > 0) {
+      setSelectedTime({ ...selectedTime, sec: difference + 1 });
+    } else if (difference < 0) {
+      setSelectedTime({ ...selectedTime, sec: 60 - difference });
+    } else {
+      setSelectedTime({ ...selectedTime, sec: 0 });
+    }
+  }, 250);
+
+  let previousScrollTopRefSec = useRef();
+
+  const eventScrollListenerSeconds = (e) => {
+    let { clientHeight, scrollTop } = e.target;
+    //update selectedTime
+    let childBoxHeight = Math.floor(clientHeight / 3);
+    debounceCalcSecChange(scrollTop, childBoxHeight);
+    //6
+    let { type, shouldAddColumn } = checkShouldAddColumn(e);
+    if (shouldAddColumn) {
+      //generate divs
+      let appendArr = [];
+      addMoreColumns("sec", appendArr);
+      //append or prepend depending on where the user scrolled
+      type === "append"
+        ? setSecColumn([...secColumnState, ...appendArr])
+        : setSecColumn([...appendArr, ...secColumnState]);
+    }
+  };
+
+  const eventScrollListenerMinutes = (e) => {
+    let { type, shouldAddColumn } = checkShouldAddColumn(e);
+    if (shouldAddColumn) {
+      let appendArr = [];
+      addMoreColumns("min", appendArr);
+      type === "append"
+        ? setMinColumn([...minColumnState, ...appendArr])
+        : setMinColumn([...appendArr, ...minColumnState]);
+    }
+  };
 
   //start the columns at the center and recenter when user reaches bottom or top
   useEffect(() => {
     //sec column centered
     let secColumns = document.getElementById("secondsColumn");
-    let {
-      clientHeight: secClientHeight,
-      scrollHeight: secScrollHeight,
-      scrollTop: secScrollTop,
-    } = secColumns;
+    let { scrollHeight: secScrollHeight } = secColumns;
     let secCenter = secScrollHeight / 2;
-    secColumns.scrollTo(0, secCenter);
+    secColumns.scrollTop = secCenter;
+    setScrollTopValue({ ...scrollTopValue, scrollTopSec: secCenter });
+    previousScrollTopRefSec.current = secCenter;
 
     //min column centered
     let minColumns = document.getElementById("minColumn");
@@ -55,43 +120,22 @@ export default function EditableTime() {
     return () => {};
   }, []);
 
-  const checkShouldAddColumn = (e) => {
-    let { scrollTop, clientHeight, scrollHeight } = e.target;
-    if (scrollTop + clientHeight >= scrollHeight) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  const eventHandlerFuncSec = (e) => {
-    let shouldAddColumn = checkShouldAddColumn(e);
-    if (shouldAddColumn) {
-      let appendArr = [];
-      addMoreColumns("sec", appendArr);
-      setSecColumn([...secColumnState, ...appendArr]);
-      e.target.removeEventListener("scroll", eventHandlerFuncSec);
-    }
-  };
-  const eventHandlerFuncMin = (e) => {
-    let shouldAddColumn = checkShouldAddColumn(e);
-    if (shouldAddColumn) {
-      let appendArr = [];
-      addMoreColumns("min", appendArr);
-      setMinColumn([...minColumnState, ...appendArr]);
-      e.target.removeEventListener("scroll", eventHandlerFuncMin);
-    }
-  };
-
   useEffect(() => {
     //recenter sec and min when hitting bottom or top of scroll
     let secColumns = document.getElementById("secondsColumn");
     let minColumns = document.getElementById("minColumn");
 
-    secColumns.addEventListener("scroll", eventHandlerFuncSec);
-    minColumns.addEventListener("scroll", eventHandlerFuncMin);
-  });
+    secColumns.addEventListener("scroll", eventScrollListenerSeconds);
+    minColumns.addEventListener("scroll", eventScrollListenerMinutes);
+    return () => {
+      secColumns.removeEventListener("scroll", eventScrollListenerSeconds);
+      minColumns.removeEventListener("scroll", eventScrollListenerMinutes);
+    };
+  }, []);
   return (
     <div className="h-1/2 ">
+      {selectedTime.min}
+      {selectedTime.sec}
       <div className="flex justify-center h-full items-center">
         <div
           id="minColumn"
