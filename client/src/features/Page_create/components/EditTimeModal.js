81dcbb9db8846618662f31ad15fe313c452decdd
Loi <loivp13@gmail.com>
2021-06-25 11:11:46 -0700

    add css transition for buttons when hiding pane

--- client/src/features/Page_create/components/EditTimeModal.js
+++ client/src/features/Page_create/components/EditTimeModal.js
@@ -2,13 +2,20 @@ import React, { useState, useEffect } from "react";
 import CloseIcon from "../../../images/BUTTON - cancel@3x.png";
 import CtaButtons from "./CtaButtons";
 import EditableTime from "./EditableTime";
+import classNames from "classnames";
 
 export default function EditTimeModal() {
   let [hidePane, setHidePane] = useState(false);
   let handleHidePaneOnFocus = () => {
     setHidePane((hidePane) => !hidePane);
   };
-
+  let transformYButtonsClass = classNames(
+    "flex",
+    "justify-around",
+    "transform",
+    "duration-1000",
+    { "-translate-y-full": hidePane }
+  );
   return (
     <div className="absolute top-0 w-screen h-screen bg-th-primary z-10 p-4 flex flex-col justify-around items-center font-openSans">
       <div className="h-full w-full max-w-md">
@@ -23,7 +30,7 @@ export default function EditTimeModal() {
           handleHidePaneOnFocus={handleHidePaneOnFocus}
           hidePane={hidePane}
         ></EditableTime>
-        <div className="flex justify-around">
+        <div className={transformYButtonsClass}>
           <CtaButtons text={"back"}></CtaButtons>
           <CtaButtons text={"save"}></CtaButtons>
         </div>
